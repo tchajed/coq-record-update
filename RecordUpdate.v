f1cb72a1a97e6c86f0e44e5356d9cf2177cefd27
Tej Chajed <tchajed@mit.edu>
2018-07-23 16:42:07 -0400

    Add some documentation and export less

--- RecordUpdate.v
+++ RecordUpdate.v
@@ -1,20 +1,36 @@
-Definition Reader E T := E -> T.
+Local Definition Reader E T := E -> T.
 
-Definition get {E} : Reader E E := fun e => e.
+Local Definition get {E} : Reader E E := fun e => e.
 
-Definition pure {E T} (x:T) : Reader E T := fun _ => x.
+Local Definition pure {E T} (x:T) : Reader E T := fun _ => x.
 
-Definition ap {E A B} (f: Reader E (A -> B)) : Reader E A -> Reader E B :=
+Local Definition ap {E A B} (f: Reader E (A -> B)) : Reader E A -> Reader E B :=
   fun x => fun e => f e (x e).
 
-Infix "<*>" := (ap) (at level 11, left associativity).
+Local Infix "<*>" := (ap) (at level 11, left associativity).
 
-Definition bind {E T T'} (x: Reader E T) (f: T -> Reader E T') : Reader E T' :=
-  fun e => f (x e) e.
+(** Updateable is a way of accessing a constructor for a record of type T. The
+syntactic form of this definition is important: it must be an eta-expanded
+version of T's constructor, written generically over the field accessors of T.
+The best way to do this for a record X := mkX { A; B; C} is [pure mkX <*> A <*>
+B <*> C]. *)
+Class Updateable T := { mkT: Reader T T;
+                        mkT_ok: forall x, mkT x = get x }.
+Arguments mkT T mk : clear implicits, rename.
 
-Notation "f =<< x" := (bind x f) (at level 0).
+Local Ltac mkUpdateable e :=
+  refine {| mkT := e |};
+  (match goal with
+   | |- forall x, _ = _ => solve [ destruct x; cbv; f_equal ]
+   end).
 
-Ltac updater etaT proj :=
+(** mkUpdateable creates an instance of Updateable from an expression like [pure
+mkX <*> A <*> B <*> C] *)
+Local Notation mkUpdateable e := (ltac:(mkUpdateable e)) (only parsing).
+
+(** [updater] creates a setter based on an eta-expanded record constructor and a
+particular field projection proj *)
+Local Ltac updater etaT proj :=
   let set :=
       (match eval pattern proj in etaT with
        | ?updater ?proj => constr:(fun x => updater (pure x))
@@ -22,34 +38,30 @@ Ltac updater etaT proj :=
   let set := (eval cbv [pure ap] in set) in
   exact set.
 
-Class Updateable T := { mkT: Reader T T;
-                        mkT_ok: forall x, mkT x = get x }.
-Arguments mkT T mk : clear implicits, rename.
+(* Combining the above, [getSetter'] looks up the eta-expanded version of T with
+the Updateable typeclass, and calls [updater] to create an updater. *)
+Local Ltac getSetter' T proj :=
+  match constr:(mkT T _) with
+  | mkT _ ?updateable =>
+    let updateable := (eval hnf in updateable) in
+    match updateable with
+    | {| mkT := ?mk |} =>
+      updater mk proj
+    end
+  end.
 
-Ltac prove_mkT_ok :=
-  match goal with
-  | |- forall x, _ = _ =>
-    solve [ destruct x; cbv; f_equal ]
-  end || fail "updater appears to change record".
+(* [getSetter] looks up the record type from the projection [proj] and then
+calls [getSetter'] *)
+Local Ltac getSetter proj :=
+  let T := match type of proj with
+           | ?T -> _ => T
+           end in
+  getSetter' T proj.
 
-Ltac mkUpdateable e :=
-  refine {| mkT := e |};
-  (match goal with
-   | |- forall x, _ = _ => solve [ destruct x; cbv; f_equal ]
-   end).
+(* [set proj] is a sets the field projected by [proj].
 
-Notation mkUpdateable e := (ltac:(mkUpdateable e)) (only parsing).
-
-Notation set T FA :=
-  (ltac:(match constr:(mkT T _) with
-         | mkT _ ?updateable =>
-           let updateable := (eval hnf in updateable) in
-           match updateable with
-           | {| mkT := ?mk |} =>
-             updater mk FA
-           end
-         end))
-    (only parsing).
+The record type T that [proj] accesses should have an instance Updateable T. *)
+Notation set proj := (ltac:(getSetter proj)) (only parsing).
 
 Module Example.
 
@@ -59,7 +71,7 @@ Module Example.
 
   Instance etaX : Updateable _ := mkUpdateable (pure mkX <*> A <*> B <*> C).
 
-  Definition setA := set X A.
+  Definition setA := set A.
   Print setA.
 
 End Example.
