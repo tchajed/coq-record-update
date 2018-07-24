4127c41a10f305b50bee83b94681a6b413f053d8
Tej Chajed <tchajed@mit.edu>
2018-07-24 12:14:46 -0400

    Consistently use Setter terminology
    Same as Haskell's lens library

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Coq has no record update syntax, nor does it create updaters for setting individual fields of a record. This small library automates creating such updaters.
 
-To use the library with a record, one must implement a typeclass `Updateable` to provide the syntax for constructing a record from individual fields. This implementation lists out the record's constructor and every field accessor function.
+To use the library with a record, one must implement a typeclass `Settable` to provide the syntax for constructing a record from individual fields. This implementation lists out the record's constructor and every field accessor function.
 
-Once `Updateable T` is implemented, Coq will be able to resolve the typeclass `Setter F` for all the fields `F` of `T`, so that a generic setter `set T A (F: T -> A) : forall {_:Setter F}, A -> T -> T` works. There is also a notation `x [proj := v]` for calling `set proj v x`.
+Once `Settable T` is implemented, Coq will be able to resolve the typeclass `Setter F` for all the fields `F` of `T`, so that a generic setter `set T A (F: T -> A) : forall {_:Setter F}, A -> T -> T` works. There is also a notation `x [proj := v]` for calling `set proj v x`.
--- RecordUpdate.v
+++ RecordUpdate.v
@@ -1,56 +1,60 @@
-Require Export ReaderApplicative.
-
 Set Implicit Arguments.
 
-(** Updateable is a way of accessing a constructor for a record of type T. The
+(** Reader is the reader monad (or just the function monad). We only use
+Applicative here. *)
+Definition Reader E T := E -> T.
+
+Definition pure {E T} (x:T) : Reader E T := fun _ => x.
+
+Definition ap {E A B} (f: Reader E (A -> B)) : Reader E A -> Reader E B :=
+  fun x => fun e => f e (x e).
+
+Infix "<*>" := (ap) (at level 11, left associativity).
+
+(** Settable is a way of accessing a constructor for a record of type T. The
 syntactic form of this definition is important: it must be an eta-expanded
 version of T's constructor, written generically over the field accessors of T.
 The best way to do this for a record X := mkX { A; B; C} is [pure mkX <*> A <*>
 B <*> C]. *)
-Class Updateable T := { mkT: Reader T T;
-                        mkT_ok: forall x, mkT x = get x }.
+Class Settable T := { mkT: Reader T T;
+                        mkT_ok: forall x, mkT x = x }.
 Arguments mkT T mk : clear implicits, rename.
 
-Local Ltac mkUpdateable e :=
+Local Ltac mkSettable e :=
   refine {| mkT := e |};
   (match goal with
    | |- forall x, _ = _ => solve [ destruct x; cbv; f_equal ]
    end).
 
-(** mkUpdateable creates an instance of Updateable from an expression like [pure
-mkX <*> A <*> B <*> C] *)
-Notation mkUpdateable e := (ltac:(mkUpdateable e)) (only parsing).
+(** mkSettable creates an instance of Settable from an expression like [pure mkX
+<*> A <*> B <*> C] *)
+Notation mkSettable e := (ltac:(mkSettable e)) (only parsing).
 
-(** [updater] creates a setter based on an eta-expanded record constructor and a
+(** [setter] creates a setter based on an eta-expanded record constructor and a
 particular field projection proj *)
-Local Ltac updater etaT proj :=
+Local Ltac setter etaT proj :=
   let set :=
       (match eval pattern proj in etaT with
-       | ?updater ?proj => constr:(fun x => updater (pure x))
+       | ?setter ?proj => constr:(fun x => setter (pure x))
        end) in
   let set := (eval cbv [pure ap] in set) in
   exact set.
 
 (* Combining the above, [getSetter'] looks up the eta-expanded version of T with
-the Updateable typeclass, and calls [updater] to create an updater. *)
-Local Ltac getSetter' T proj :=
+the Settable typeclass, and calls [setter] to create a setter. *)
+Local Ltac get_setter T proj :=
   match constr:(mkT T _) with
   | mkT _ ?updateable =>
     let updateable := (eval hnf in updateable) in
     match updateable with
     | {| mkT := ?mk |} =>
-      updater mk proj
+      setter mk proj
     end
   end.
 
-(* [getSetter] looks up the record type from the projection [proj] and then
-calls [getSetter'] *)
-Local Ltac getSetter proj :=
-  let T := match type of proj with
-           | ?T -> _ => T
-           end in
-  getSetter' T proj.
-
+(* Setter provides a way to change a field given by a projection function, along
+with correctness conditions that require the projected field and only the
+projected field is modified. *)
 Class Setter {R T} (proj: R -> T) :=
   { set: T -> R -> R;
     set_get: forall v r, proj (set v r) = v;
@@ -64,8 +68,8 @@ Notation "x [ proj := v ]" := (set proj v x)
 
 Ltac SetInstance_t :=
   match goal with
-  | |- Setter ?A => unshelve eapply Build_Setter;
-                  [ getSetter A | intros ? r; destruct r | intros r; destruct r ];
+  | |- @Setter ?T _ ?A => unshelve eapply Build_Setter;
+                  [ get_setter T A | intros ? r; destruct r | intros r; destruct r ];
                   intros; reflexivity
   end.
 
@@ -77,7 +81,7 @@ Module SimpleExample.
                     B: nat;
                     C: unit }.
 
-  Instance etaX : Updateable _ := mkUpdateable (pure mkX <*> A <*> B <*> C).
+  Instance etaX : Settable _ := mkSettable (pure mkX <*> A <*> B <*> C).
 
   Definition setAB a b x := x[A := a][B := b].
 
@@ -89,8 +93,8 @@ Module IndexedType.
                         C: unit }.
   Arguments X T : clear implicits.
 
-  Instance etaX T: Updateable (X T) :=
-    mkUpdateable (pure (mkX (T:=T)) <*> A <*> B <*> C).
+  Instance etaX T: Settable (X T) :=
+    mkSettable (pure (mkX (T:=T)) <*> A <*> B <*> C).
 
   Definition setAB T a b (x: X T) := x[A := a][B := b].
 
