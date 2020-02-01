88f24f501716616a58fbc6779ae52ff9184e0b74
Tej Chajed <tchajed@mit.edu>
2020-02-01 11:56:48 -0500

    Don't use constructor for constant function

--- src/RecordSet.v
+++ src/RecordSet.v
@@ -107,7 +107,7 @@ Hint Extern 1 (SetterWf _) => SetterWfInstance_t : typeclass_instances.
 Module RecordSetNotations.
   Delimit Scope record_set with rs.
   Open Scope rs.
-  Notation "x <| proj  :=  v |>" := (set proj (constructor v) x)
+  Notation "x <| proj  :=  v |>" := (set proj (fun _ => v) x)
                                     (at level 12, left associativity) : record_set.
   Notation "x <| proj  ::=  f |>" := (set proj f x)
                                      (at level 12, f at next level, left associativity) : record_set.
