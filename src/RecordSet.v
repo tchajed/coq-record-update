30bfb9f5652dbc4e577f029ad0c489655cc0cce6
Tej Chajed <tchajed@gmail.com>
2024-08-13 08:48:19 -0500

    Fix the warning in a backwards compatible way

--- src/RecordSet.v
+++ src/RecordSet.v
@@ -95,8 +95,7 @@ Module RecordSetNotations.
     (set proj1 (set proj2 .. (set projn f) ..) x)
     (at level 12, f at next level, left associativity) : record_set.
   (* TODO: this notation and the one above have a common prefix. They do still
-  have an effect, do disable the warning for now. *)
-  #[warning="-notation-incompatible-prefix"]
+  have an effect, so the warning is disabled for now. *)
   Notation "x <| proj1 ; proj2 ; .. ; projn := v |>" :=
     (set proj1 (set proj2 .. (set projn (fun _ => v)) ..) x)
     (at level 12, left associativity) : record_set.
